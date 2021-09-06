2a92d5c564f588502f9c96dab9fed7828dceb22a
Eric <acosta.eric40@gmail.com>
2021-09-06 17:54:41 -0300

    files

--- gmov/settings.py
+++ gmov/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'gmov.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Posts(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='static/images')
+    title = models.CharField(max_length=500)
+    slug = models.SlugField()
+    content = models.TextField(max_length=20000)
+    date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+    def save(self, *args, **kwargs): # new
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
