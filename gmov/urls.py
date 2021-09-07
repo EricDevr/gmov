99170c6d495fc7c0e147288e43a022c3d70aa9d2
EricDevr <acosta.eric40@gmail.com>
2021-09-06 22:15:34 -0300

    posts

--- gmov/urls.py
+++ gmov/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home.views import home
+from home.views import home,publicar
 
 urlpatterns = [
     path('', home, name='home'),
+    path('publicar/', publicar, name='publicar'),
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-
+from .models import Posts
+from .forms import PostForm
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    posts = Posts.objects.all().order_by("-id")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
+
+def publicar(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            message = "post publicado"
+        else:
+            message = "error al publicar"
+        form = PostForm()
+    else:
+        message = "publicar en gmov"
+        form = PostForm()
+    context = {
+        'message': message,
+        'form': form,
+    }
+    return render(request, 'publicar.html', context)
